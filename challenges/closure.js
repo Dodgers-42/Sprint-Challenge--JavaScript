f383edd8c80504a58e03103e355b4ec3e25284e0
Dodger-42 <kevin.bing.barber@gmail.com>
2020-05-15 17:47:23 -0700

    first

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,22 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+It can access the variable internal because it's console.logging it. And its on itnernal and not the Global Scope.
 
 /* Task 2: Counter */
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function sumation(num) {
+  let counter = 1;
+  return function() {
+      for (num; num > 1; num--) {
+          counter = counter + num
+      }
+      return counter;
+  }
+}
+
+const adding = sumation(4);
+
+console.log(adding());
